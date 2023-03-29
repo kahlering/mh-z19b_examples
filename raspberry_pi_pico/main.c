c3e469f825473710866b97d21a87532fc76019c3
kahlering <s_bo8dc2@uni-bremen.de>
2023-03-29 23:02:48 +0200

    Update main.c

--- raspberry_pi_pico/main.c
+++ raspberry_pi_pico/main.c
@@ -5,11 +5,10 @@
 
 #include "mh-z19b.h"
 
+
 #define UART_ID uart0
 #define BAUD_RATE 9600
 
-// We are using pins 0 and 1, but see the GPIO function select table in the
-// datasheet for information on which other pins can be used.
 #define UART_TX_PIN 0
 #define UART_RX_PIN 1
 
@@ -41,14 +40,13 @@ void uart_discard_input(void){
 }
 
 int main() {
-	stdio_init_all();
+    stdio_init_all();
 	
 	
-	// Set up our UART with the required speed.
+    // Set up our UART with the required speed.
     uart_init(UART_ID, BAUD_RATE);
 
     // Set the TX and RX pins by using the function select on the GPIO
-    // Set datasheet for more information on function select
     gpio_set_function(UART_TX_PIN, GPIO_FUNC_UART);
     gpio_set_function(UART_RX_PIN, GPIO_FUNC_UART);
 
